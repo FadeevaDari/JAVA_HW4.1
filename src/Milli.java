474b9dcc8fc39e3ab7496c0316ee05f7459b9c84
Daria Fadeeva <9119046505@mail.ru>
2020-08-30 00:28:30 +0300

    Added Milli

---
+++ src/Milli.java
@@ -0,0 +1,8 @@
+public class Milli {
+    public static void main(String[] args) {
+        int ticket = 36410;
+        int bonus = 20;
+        int milli = ticket / bonus;
+        System.out.println(milli);
+    }
+}
